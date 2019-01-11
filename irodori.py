288ca0ea339c5fa8604bad3bd50d95ca88e4d346
Gerbera <46430027+gerberasd@users.noreply.github.com>
2019-01-12 07:55:22 +0900

    Ver 0.0.1
    "Hello, world."

---
+++ irodori.py
@@ -0,0 +1,36 @@
+import discord #おまじない
+
+client = discord.Client() #おまじない
+
+#            ___ ____   ___  ____   ___  ____  ___ 
+#           |_ _|  _ \ / _ \|  _ \ / _ \|  _ \|_ _|
+#            | || |_) | | | | | | | | | | |_) || | 
+#            | ||  _ <| |_| | |_| | |_| |  _ < | | 
+#           |___|_| \_\\___/|____/ \___/|_| \_\___|
+#           Ver.0.0.1
+#           
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    print('    _/_/_/  _/_/_/      _/_/    _/_/_/      _/_/    _/_/_/    _/_/_/   ')
+    print('     _/    _/    _/  _/    _/  _/    _/  _/    _/  _/    _/    _/      ')
+    print('    _/    _/_/_/    _/    _/  _/    _/  _/    _/  _/_/_/      _/       ')
+    print('   _/    _/    _/  _/    _/  _/    _/  _/    _/  _/    _/    _/        ')
+    print('_/_/_/  _/    _/    _/_/    _/_/_/      _/_/    _/    _/  _/_/_/       ')
+    print('Ver.0.0.1')
+    print('------')
+    # 起動確認
+
+@client.event
+async def on_message(message):
+    # 「!test」で始まる場合
+    if message.content.startswith("!test2"):
+        m = "HelloWorld!"
+        # メッセージが送られてきたチャンネルへHelloWorld!と送信
+        await client.send_message(message.channel, m)
+
+client.run("NTMzMzg3MDgzMjU2MTY4NDc5.DxqtjQ.YgjNMXWb-eXoZs7Q2aClA6nuxDo") #トークンを貼り付けて下さい
